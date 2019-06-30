6eb813eba53c2a1bb0f902d1b684c77172a257ad
chunza2542 <chunza2542@hotmail.com>
2019-06-30 10:37:53 +0700

    Setup Loading Bar

---
+++ src/Randomizer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: black;
+  top: 0;
+  left: 0;
+`;
+
+const Randomizer = ({src}) => {
+  return <Container></Container>;
+};
+
+export default Randomizer;
--- src/Uploader.js
+++ src/Uploader.js
@@ -1,5 +1,8 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import styled from "styled-components";
+import ReactLoading from "react-loading";
+
+import Randomizer from "./Randomizer";
 
 const UploadButton = styled.div`
   padding: 10px 0;
@@ -12,18 +15,79 @@ const UploadButton = styled.div`
   font-size: 1.4em;
 `;
 
+const Loader = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.9);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  & span {
+    margin-top: 20px;
+    font-size: 1.4rem;
+  }
+`;
+
 const Uploader = () => {
   const uploadInput = useRef(null);
+  const [file, setFile] = useState(null);
+  const [result, setResult] = useState({});
+
+  const [loading, setLoading] = useState(0);
+
+  function handleUploader(e) {
+    recognize(e.target.files[0]);
+  }
+
+  async function recognize(file) {
+    const tesseract = window.Tesseract;
+
+    setLoading(1);
+
+    const result = await tesseract
+      .recognize(file, {
+        lang: "eng",
+      })
+      .progress(({progress, status}) => {
+        if (!progress || !status || status !== "recognizing text") {
+          return null;
+        }
+
+        setLoading(Math.floor(progress * 100));
+      });
+
+    setFile(URL.createObjectURL(file));
+    setResult(result);
+
+    setLoading(0);
+  }
 
   return (
     <div>
+      {loading !== 0 && (
+        <Loader>
+          <ReactLoading type="spin" color="black" width="80px" height="80px" />
+          <span>Thinking.... {loading}%</span>
+        </Loader>
+      )}
+
       <input
         ref={uploadInput}
         style={{opacity: 0}}
         type="file"
         accept="image/*"
         capture="camera"
+        onChange={handleUploader}
       />
+
+      {file && result && <Randomizer src={file} result={result} />}
 
       <UploadButton
         onClick={() => {
